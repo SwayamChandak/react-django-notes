dc2e56ef5117465d8100e80af8df22a8e33a4ee4
SwayamChandak <chandakswayam3904@gmail.com>
2025-05-26 20:22:44 +0530

    frontend added

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,12 @@
+// import api from "../api"
+import Form from "../components/Form"
+
+function Register(){
+    return(
+        <>
+        <Form route="/api/user/register/" method="register" />
+        </>
+    )
+}
+
+export default Register
